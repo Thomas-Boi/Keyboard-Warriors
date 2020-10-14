835b1e017733b03e5d393c70f23eb91fe2ba7c72
Thomas Bui <thomasbui198@gmail.com>
2020-10-14 09:59:17 -0700

    Added MenuScene

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // exit the game
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
