30e62443604963cb6853bbf965254e60d13c3968
Sebastian Bejm <125sbejm@gmail.com>
2020-11-06 13:02:56 -0800

    Added Shop UI

--- Assets/Scripts/UI/ItemShopScene/UpdateShopUI.cs
+++ Assets/Scripts/UI/ItemShopScene/UpdateShopUI.cs
@@ -7,11 +7,26 @@ public class UpdateShopUI : MonoBehaviour
 {
 
     public Text moneyTxtObj;
+    public List<Button> itemOptions;
+
+    private ProgressTracker tracker;
+    private ItemTracker itemTracker;
 
     void Start()
     {
-        var tracker = ProgressTracker.GetTracker();
-        moneyTxtObj.text = ItemTracker.GetTracker().Money.ToString();
+        tracker = ProgressTracker.GetTracker();
+        itemTracker = ItemTracker.GetTracker();
+    }
+
+    void Update()
+    {
+        UpdateMoneyText();
+
+    }
+
+    private void UpdateMoneyText()
+    {
+        moneyTxtObj.text = itemTracker.Money.ToString();
     }
 
 }
