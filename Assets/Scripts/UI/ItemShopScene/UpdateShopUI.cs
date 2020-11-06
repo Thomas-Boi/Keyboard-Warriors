a781218e2688b243c2052cf74be25c2236a0dfcb
Sebastian Bejm <125sbejm@gmail.com>
2020-11-06 12:06:04 -0800

    Created ItemShopScene

--- Assets/Scripts/UI/ItemShopScene/UpdateShopUI.cs
+++ Assets/Scripts/UI/ItemShopScene/UpdateShopUI.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UpdateUI : MonoBehaviour
+public class UpdateShopUI : MonoBehaviour
 {
-    // the text objects
-    public Text weekTxtObj;
+
     public Text moneyTxtObj;
 
-    // Start is called before the first frame update
-    // update the player's progress on the HomeScene
     void Start()
     {
         var tracker = ProgressTracker.GetTracker();
-        weekTxtObj.text = $"Week {tracker.WeekNum} / {ProgressTracker.finalWeekNum}";
+    }
+
+    void Update()
+    {
         moneyTxtObj.text = ItemTracker.GetTracker().Money.ToString();
     }
+
 }
